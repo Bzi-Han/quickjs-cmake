512ecffaa076fa87cbaa093acca1d89b3ab1059a
Bzi_Han <rosebzi@foxmail.com>
2024-03-07 17:34:00 +0800

    Fix add initialize value for MSVC runtime check

--- quickjs/quickjs.c
+++ quickjs/quickjs.c
@@ -24132,7 +24132,7 @@ static int js_parse_destructuring_element(JSParseState *s, int tok, int is_arg,
                                         int hasval, int has_ellipsis,
                                         BOOL allow_initializer)
 {
-    int label_parse, label_assign, label_done, label_lvalue, depth_lvalue;
+    int label_parse, label_assign, label_done, label_lvalue = 0, depth_lvalue;
     int start_addr, assign_addr;
     JSAtom prop_name, var_name;
     int opcode, scope, tok1, skip_bits;
